b13806fcfb931cb18bbed3d3f0ff054ea843d148
iarivera <iariveradev@outlook.com>
2024-11-17 10:54:33 -0500

    Revert "added bare minimum functions to main menu"
    This reverts commit 9fa5643b14012fbdbe7b22041953d3c04f5142f7.

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -9,7 +9,3 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
-func _on_quit_btn_pressed() -> void:
-	get_tree().quit()
