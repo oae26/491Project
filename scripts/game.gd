a8438f14331cc82e674ccfdf8ec0345df49ed5c8
oae26 <oae26@njit.edu>
2024-11-20 14:54:29 -0500

    kumar presentation update

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,8 +1,8 @@
 extends Node2D
 
 
-@onready var pause_menu = $PauseMenu
-@onready var resume = $PauseMenu/Panel/VBoxContainer/ResumeBtn
+@onready var pause_menu = $Player/Camera2D/CanvasLayer/PauseMenu
+@onready var resume = $Player/Camera2D/CanvasLayer/PauseMenu/Panel/VBoxContainer/ResumeBtn
 var paused = false
 # Called when the node enters the scene tree for the first time.
 
